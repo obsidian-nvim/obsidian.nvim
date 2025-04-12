7f29c66ced2114f92c54daaae31db8e029e4ef95
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-04-12 17:52:01 +0800

    feat: tags completion, better hover link recognition

--- lua/obsidian/init.lua
+++ lua/obsidian/init.lua
@@ -145,11 +145,15 @@ obsidian.setup = function(opts)
       end
 
       -- Inject completion sources, providers to their plugin configurations
-      if opts.completion.nvim_cmp then
-        require("obsidian.completion.plugin_initializers.nvim_cmp").inject_sources()
-      elseif opts.completion.blink then
-        require("obsidian.completion.plugin_initializers.blink").inject_sources()
-      end
+      -- if opts.completion.nvim_cmp then
+      --   require("obsidian.completion.plugin_initializers.nvim_cmp").inject_sources()
+      -- elseif opts.completion.blink then
+      --   require("obsidian.completion.plugin_initializers.blink").inject_sources()
+      -- end
+
+      vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+      vim.bo[ev.buf].completeopt = "menu,menuone,noselect"
+      require("obsidian.lsp").start()
 
       local win = vim.api.nvim_get_current_win()
 
@@ -164,7 +168,7 @@ obsidian.setup = function(opts)
 
       -- Run enter-note callback.
       client.callback_manager:enter_note(function()
-        return obsidian.Note.from_buffer(ev.bufnr)
+        return obsidian.Note.from_buffer(ev.buf)
       end)
     end,
   })
