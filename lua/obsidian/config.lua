3917e87ace67a1dd3fed210f969af070204cf078
Magnus <magnus.rinnan@gmail.com>
2025-05-01 14:57:21 +0200

    feat: Add `match_case` option, to avoid duplicates.
    Defaulting to `true`, to keep existing behavior.
    
    Closes #90
    
    Changes to be committed:
          modified:   lua/obsidian/completion/sources/base/refs.lua
          modified:   lua/obsidian/config.lua

--- lua/obsidian/config.lua
+++ lua/obsidian/config.lua
@@ -289,6 +289,7 @@ config.LinkStyle = {
 ---@field nvim_cmp boolean
 ---@field blink boolean
 ---@field min_chars integer
+---@field match_case boolean
 config.CompletionOpts = {}
 
 --- Get defaults.
@@ -299,6 +300,7 @@ config.CompletionOpts.default = function()
   return {
     nvim_cmp = has_nvim_cmp,
     min_chars = 2,
+    match_case = true,
   }
 end
 
