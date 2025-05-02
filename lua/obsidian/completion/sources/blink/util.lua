9e34f07551c5bd9fb62c577428f6a2c94652c8e5
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-05-02 22:28:50 +0800

    fix: correct line before and after

--- lua/obsidian/completion/sources/blink/util.lua
+++ lua/obsidian/completion/sources/blink/util.lua
@@ -6,8 +6,8 @@ local M = {}
 M.generate_completion_request_from_editor_state = function(context)
   local row = context.cursor[1]
   local col = context.cursor[2] + 1
-  local cursor_before_line = context.line:sub(1, col)
-  local cursor_after_line = context.line:sub(col + 1)
+  local cursor_before_line = context.line:sub(1, col - 1)
+  local cursor_after_line = context.line:sub(col)
 
   return {
     context = {
