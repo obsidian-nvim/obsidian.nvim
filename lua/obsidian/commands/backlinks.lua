e397b1647178fb6349cfd43d4a012b9a6a0526df
Sebastian Szyller <contact@sebszyller.com>
2025-05-22 15:18:24 +0300

    feat: better comment

--- lua/obsidian/commands/backlinks.lua
+++ lua/obsidian/commands/backlinks.lua
@@ -121,7 +121,7 @@ return function(client)
 
     local note = client:current_note(0, load_opts)
 
-    -- Check if cursor is on a header, if so, use that anchor.
+    -- Check if cursor is on a header, if so and header parsing is enabled, use that anchor.
     if client.opts.backlinks.parse_headers then
       local header_match = util.parse_header(vim.api.nvim_get_current_line())
       if header_match then
