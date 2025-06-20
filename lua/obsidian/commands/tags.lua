1377f5b0da1726767e6affaf51c6050636b3a20a
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-06-20 22:39:31 +0800

    prototype: massively simplify finding tags, backlinks, links

--- lua/obsidian/commands/tags.lua
+++ lua/obsidian/commands/tags.lua
@@ -2,75 +2,32 @@ local log = require "obsidian.log"
 local util = require "obsidian.util"
 local api = require "obsidian.api"
 local find = require "obsidian.find"
+local Note = require "obsidian.note"
 
----@param client obsidian.Client
 ---@param picker obsidian.Picker
 ---@param tags string[]
-local function _gather_tag_picker_list(client, picker, tags)
-  client:find_tags_async(tags, function(tag_locations)
-    -- Format results into picker entries, filtering out results that aren't exact matches or sub-tags.
-    ---@type obsidian.PickerEntry[]
-    local entries = {}
-    for _, tag_loc in ipairs(tag_locations) do
+local function gather_tag_picker_list(picker, tags)
+  local entries = {}
+
+  find.tags(tags, {
+    on_match = function(tag_loc)
       for _, tag in ipairs(tags) do
         if tag_loc.tag == tag or vim.startswith(tag_loc.tag, tag .. "/") then
-          local display = string.format("%s [%s] %s", tag_loc.note:display_name(), tag_loc.line, tag_loc.text)
+          local note = Note.from_file(tag_loc.path)
+          local display = string.format("%s [%s] %s", note:display_name(), tag_loc.line, tag_loc.tag)
           entries[#entries + 1] = {
             value = { path = tag_loc.path, line = tag_loc.line, col = tag_loc.tag_start },
             display = display,
             ordinal = display,
-            filename = tostring(tag_loc.path),
+            filename = tag_loc.path,
             lnum = tag_loc.line,
             col = tag_loc.tag_start,
           }
           break
         end
       end
-    end
-
-    if vim.tbl_isempty(entries) then
-      if #tags == 1 then
-        log.warn "Tag not found"
-      else
-        log.warn "Tags not found"
-      end
-      return
-    end
-
-    vim.schedule(function()
-      picker:pick(entries, {
-        prompt_title = "#" .. table.concat(tags, ", #"),
-        callback = function(value)
-          api.open_buffer(value.path, { line = value.line, col = value.col })
-        end,
-      })
-    end)
-  end, { search = { sort = true } })
-end
-
----@param client obsidian.Client
----@param picker obsidian.Picker
----@param tags string[]
-local function gather_tag_picker_list(client, picker, tags)
-  local entries = {}
-
-  find.find_tag(tags, function(tag_loc)
-    for _, tag in ipairs(tags) do
-      if tag_loc.tag == tag or vim.startswith(tag_loc.tag, tag .. "/") then
-        -- local display = string.format("%s [%s] %s", tag_loc.note:display_name(), tag_loc.line, tag_loc.text)
-        local display = string.format("%s [%s] %s", tag_loc.path, tag_loc.line, tag_loc.tag)
-        entries[#entries + 1] = {
-          value = { path = tag_loc.path, line = tag_loc.line, col = tag_loc.tag_start },
-          display = display,
-          ordinal = display,
-          filename = tag_loc.path,
-          lnum = tag_loc.line,
-          col = tag_loc.tag_start,
-        }
-        break
-      end
-    end
-  end, function()
+    end,
+  }, function()
     if vim.tbl_isempty(entries) then
       if #tags == 1 then
         log.warn "Tag not found"
@@ -110,15 +67,14 @@ return function(client, data)
   end
 
   if not vim.tbl_isempty(tags) then
-    return gather_tag_picker_list(client, picker, util.tbl_unique(tags))
+    return gather_tag_picker_list(picker, util.tbl_unique(tags))
   else
-    client:list_tags_async(nil, function(all_tags)
+    find.list_tags(function(all_tags)
       vim.schedule(function()
         -- Open picker with tags.
         picker:pick_tag(all_tags, {
           callback = function(...)
-            print(...)
-            gather_tag_picker_list(client, picker, { ... })
+            gather_tag_picker_list(picker, { ... })
           end,
           allow_multiple = true,
         })
