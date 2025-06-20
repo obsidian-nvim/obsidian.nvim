20de1c54acc4e8d10251aa122857a809f8266e8a
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-06-20 22:39:31 +0800

    prototype: massively simplify finding tags, backlinks, links

--- lua/obsidian/find.lua
+++ lua/obsidian/find.lua
@@ -13,32 +13,38 @@ local function strip_tag(s)
   return s
 end
 
----@param opts obsidian.SearchOpts|boolean|?
+local default = {
+  sort = false,
+  include_templates = false,
+  ignore_case = false,
+}
+
+---@param opts obsidian.SearchOpts
 ---@param additional_opts obsidian.search.SearchOpts|?
 ---
 ---@return obsidian.search.SearchOpts
 ---
 ---@private
 local prepare_search_opts = function(opts, additional_opts)
-  local client = require("obsidian").get_client()
-  opts = client:_search_opts_from_arg(opts)
+  local plugin_opts = require("obsidian").get_client().opts
+  opts = vim.tbl_extend("keep", opts, default)
 
   local search_opts = {}
 
   if opts.sort then
-    search_opts.sort_by = client.opts.sort_by
-    search_opts.sort_reversed = client.opts.sort_reversed
+    search_opts.sort_by = plugin_opts.sort_by
+    search_opts.sort_reversed = plugin_opts.sort_reversed
   end
 
-  if not opts.include_templates and client.opts.templates ~= nil and client.opts.templates.folder ~= nil then
-    search.SearchOpts.add_exclude(search_opts, tostring(client.opts.templates.folder))
+  if not opts.include_templates and plugin_opts.templates ~= nil and plugin_opts.templates.folder ~= nil then
+    search.SearchOpts.add_exclude(search_opts, tostring(plugin_opts.templates.folder))
   end
 
   if opts.ignore_case then
     search_opts.ignore_case = true
   end
 
-  if additional_opts ~= nil then
+  if additional_opts then
     search_opts = search.SearchOpts.merge(search_opts, additional_opts)
   end
 
@@ -48,7 +54,7 @@ end
 ---@class obsidian._TagLocation
 ---
 ---@field tag string The tag found.
----@field path string|obsidian.Path The path to the note where the tag was found.
+---@field path string The path to the note where the tag was found.
 ---@field line integer The line number (1-indexed) where the tag was found.
 ---@field tag_start integer|? The index within 'text' where the tag starts.
 ---@field tag_end integer|? The index within 'text' where the tag ends.
@@ -56,41 +62,51 @@ end
 -- TODO: query properly ignore_case, music and Music is same tag
 
 ---@param query string | table
----@param on_match fun(tag_loc: obsidian._TagLocation)
+---@param opts { dir: string, on_match: fun(tag_loc: obsidian._TagLocation) }
 ---@param on_finish fun(tag_locs: obsidian._TagLocation[])
----@param opts? { dir: string }
 ---@async
-local function find_tags(query, on_match, on_finish, opts)
+local function find_tags(query, opts, on_finish)
   vim.validate("query", query, { "string", "table" })
-  vim.validate("on_match", on_match, "function")
   vim.validate("on_finish", on_finish, "function")
   opts = opts or {}
-  opts = vim.tbl_extend("keep", opts, { dir = tostring(require("obsidian").get_client().dir) })
+  opts.dir = opts.dir or require("obsidian").get_client().dir
 
   local terms = type(query) == "string" and { query } or query ---@cast terms -string
 
   local search_terms = {}
   for _, term in ipairs(terms) do
-    search_terms[#search_terms + 1] = "\\B#([\\p{L}\\p{N}_-]*" .. term .. "[\\p{L}\\p{N}_-]*)" -- tags in the wild
+    if term ~= "" then
+      -- Match tags that contain the term
+      search_terms[#search_terms + 1] = "\\B#([\\p{L}\\p{N}_-]*" .. term .. "[\\p{L}\\p{N}_-]*)" -- tags in the wild
+    else
+      -- Match any valid non-empty tag
+      search_terms[#search_terms + 1] = "\\B#([\\p{L}\\p{N}_-]+)"
+    end
   end
 
   local results = {}
 
-  search.search_async(opts.dir, search_terms, { ignore_case = true }, function(match) -- TODO: prepare_search_opts
+  search.search_async(opts.dir, search_terms, { ignore_case = true }, function(match)
     local tag_match = match.submatches[1]
+    local match_text = tag_match.match.text
+
+    if util.is_hex_color(match_text) then
+      return
+    end
 
     ---@type obsidian._TagLocation
     local tag_loc = {
       tag_start = tag_match.start,
       tag_end = tag_match["end"],
-      tag = strip_tag(tag_match.match.text),
+      tag = strip_tag(match_text),
       path = match.path.text,
       line = match.line_number,
     }
     table.insert(results, tag_loc)
-    on_match(tag_loc)
+    if opts.on_match then
+      opts.on_match(tag_loc)
+    end
   end, function(exit_code)
-    assert(exit_code == 0)
     on_finish(results)
   end)
 end
@@ -98,9 +114,11 @@ end
 local function list_tags(callback)
   vim.validate("callback", callback, "function")
   local found = {}
-  find_tags("", function(tag_loc)
-    found[tag_loc.tag] = true
-  end, function(tag_locs)
+  find_tags("", {
+    on_match = function(tag_loc)
+      found[tag_loc.tag] = true
+    end,
+  }, function(tag_locs)
     callback(vim.tbl_keys(found))
   end)
 end
@@ -242,26 +260,46 @@ local function find_backlinks(note, opts, callback)
   search.search_async(
     opts.dir,
     build_backlink_search_term(note, anchor, block),
-    prepare_search_opts(opts.search, { fixed_strings = true, ignore_case = true }),
+    { fixed_strings = true, ignore_case = true },
     _on_match,
-    function(code)
-      assert(code == 0)
+    function()
       callback(results)
     end
   )
 end
 
+---@class obsidian.LinkMatch
+---@field link string
+---@field line integer
+
+---@param note obsidian.Note
+---@param opts { on_match: fun(link: obsidian.LinkMatch) }
+---@param callback fun(links: obsidian.LinkMatch[])
 local function find_links(note, opts, callback)
   -- Gather all unique raw links (strings) from the buffer.
-  ---@type table<string, integer>
-  local links = {}
+  ---@type obsidian.LinkMatch[]
+  local matches = {}
+  ---@type table<string, boolean>
+  local found = {}
+  local n_lines = vim.api.nvim_buf_line_count(0)
   for lnum, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, -1, true)) do
-    for match in vim.iter(search.find_refs(line, { include_naked_urls = true, include_file_urls = true })) do
-      local m_start, m_end = unpack(match)
+    for ref_match in vim.iter(search.find_refs(line, { include_naked_urls = true, include_file_urls = true })) do
+      local m_start, m_end = unpack(ref_match)
       local link = string.sub(line, m_start, m_end)
-      if not links[link] then
-        links[link] = lnum
+      if not found[link] then
+        local match = {
+          link = link,
+          line = lnum,
+        }
+        matches[#matches + 1] = match
+        found[link] = true
+        if opts.on_match then
+          opts.on_match(match)
+        end
       end
+    end
+    if n_lines == line then
+      callback(matches)
     end
   end
 end
@@ -269,4 +307,6 @@ end
 return {
   tags = find_tags,
   backlinks = find_backlinks,
+  links = find_links,
+  list_tags = list_tags,
 }
