29565dd230748e814f3c3d4778e1bfeaf8cdcc4e
Zizhou Teng <412445606@qq.com>
2025-06-26 16:26:21 +0800

    fix: inline some tbl methods

--- lua/obsidian/log.lua
+++ lua/obsidian/log.lua
@@ -64,53 +64,6 @@ log.log = function(msg, level, ...)
   end
 end
 
----@param msg string
----@param level integer
-log.lazy_log = function(msg, level, ...)
-  msg = string.format(tostring(msg), unpack(message_args(msg, ...)))
-  buffer[#buffer + 1] = { msg = msg, level = level }
-end
-
---- Flush lazy logs.
----
----@param opts { raw_print: boolean|? }|?
-log.flush = function(opts)
-  local util = require "obsidian.util"
-
-  opts = opts or {}
-
-  ---@type integer|?
-  local level
-  ---@type string[]
-  local messages = {}
-  local n = #buffer
-  for i, record in ipairs(buffer) do
-    if level ~= nil and level ~= record.level then
-      -- flush messages for current log level
-      if opts.raw_print then
-        print(table.concat(messages, "\n"))
-      else
-        log.log(table.concat(messages, "\n"), level)
-      end
-      util.tbl_clear(messages)
-    end
-
-    messages[#messages + 1] = record.msg
-    level = record.level
-
-    if i == n then
-      -- flush remaining messages
-      if opts.raw_print then
-        print(table.concat(messages, "\n"))
-      else
-        log.log(table.concat(messages, "\n"), level)
-      end
-    end
-  end
-
-  util.tbl_clear(buffer)
-end
-
 ---Log a message only once.
 ---
 ---@param msg any
@@ -138,17 +91,9 @@ log.info = function(msg, ...)
   log.log(msg, vim.log.levels.INFO, ...)
 end
 
-log.lazy_info = function(msg, ...)
-  log.lazy_log(msg, vim.log.levels.INFO, ...)
-end
-
 ---@param msg string
 log.warn = function(msg, ...)
   log.log(msg, vim.log.levels.WARN, ...)
-end
-
-log.lazy_warn = function(msg, ...)
-  log.lazy_log(msg, vim.log.levels.WARN, ...)
 end
 
 ---@param msg string
@@ -169,11 +114,5 @@ log.err_once = function(msg, ...)
 end
 
 log.error_once = log.err
-
-log.lazy_err = function(msg, ...)
-  log.lazy_log(msg, vim.log.levels.ERROR, ...)
-end
-
-log.lazy_error = log.lazy_err
 
 return log
