a8f5b701c17d4d8e5794c84f3313758873e3a83c
neo451 <412444506@qq.com>
2025-08-22 21:06:19 +0800

    feat: lsp document_symbols

---
+++ lua/obsidian/lsp/handlers/document_symbol.lua
@@ -0,0 +1,29 @@
+local api = require "obsidian.api"
+
+---@param params lsp.DocumentSymbolParams
+return function(params, handler)
+  local note = assert(api.current_note(0, { collect_anchor_links = true }))
+
+  ---@type lsp.DocumentSymbol[]
+  local symbols = {}
+  for _, anchor in pairs(note.anchor_links) do
+    local display = string.rep("#", anchor.level) .. " " .. anchor.header
+    local rge = {
+      start = { line = anchor.line - 1, character = 0 },
+      ["end"] = { line = anchor.line - 1, character = 0 },
+    }
+    symbols[#symbols + 1] = {
+      name = display,
+      kind = 1,
+      filename = note.path.filename,
+      range = rge,
+      selectionRange = rge,
+    }
+  end
+
+  table.sort(symbols, function(a, b)
+    return a.range.start.line < b.range.start.line
+  end)
+
+  handler(nil, symbols)
+end
