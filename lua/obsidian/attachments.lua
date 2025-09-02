38042ef9396863f3a8c3e308e9238b975a4499a1
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-09-02 13:47:39 +0800

    feat: open any attachments

---
+++ lua/obsidian/attachments.lua
@@ -0,0 +1,36 @@
+---@enum obsidian.attachment.ft
+local filetypes = {
+  -- markdown
+  "md",
+  -- json canvas
+  "canvas",
+  -- images
+  "avif",
+  "bmp",
+  "gif",
+  "jpg",
+  "jpeg",
+  "png",
+  "svg",
+  "webp",
+  -- audio
+  "flac",
+  "m4a",
+  "mp3",
+  "ogg",
+  "wav",
+  "webm",
+  "3gp",
+  -- video
+  "mkv",
+  "mov",
+  "mp4",
+  "ogv",
+  "webm",
+  -- pdf
+  "pdf",
+}
+
+return {
+  filetypes = filetypes,
+}
