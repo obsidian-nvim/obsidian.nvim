c157b6ba76daae140356a20f5af5f093bd3e1505
neo451 <412444506@qq.com>
2025-09-12 00:39:43 +0800

    fix: cleanup resolve_link and create note in definition

--- lua/obsidian/lsp/handlers/definition.lua
+++ lua/obsidian/lsp/handlers/definition.lua
@@ -1,6 +1,18 @@
 local RefTypes = require("obsidian.search").RefTypes
 local api = require "obsidian.api"
 local search = require "obsidian.search"
+local Note = require "obsidian.note"
+
+local IsNote = {
+  [RefTypes.WikiWithAlias] = true,
+  [RefTypes.Wiki] = true,
+  [RefTypes.Markdown] = true,
+}
+
+local IsWiki = {
+  [RefTypes.WikiWithAlias] = true,
+  [RefTypes.Wiki] = true,
+}
 
 ---@param params lsp.ReferenceParams
 ---@param handler fun(_:any, loactions: lsp.Location[])
@@ -10,27 +22,51 @@ return function(params, handler)
     return
   end
 
-  local link_match = search.resolve_link(cur_link, {})
-  if not link_match then
+  local match = search.resolve_link(cur_link, {})
+  if not match then
     return
   end
 
-  if link_type ~= RefTypes.NakedUrl and link_type ~= RefTypes.FileUrl and link_type ~= RefTypes.BlockID then
-    local line = link_match.line and link_match.line - 1 or 0
-    local uri = vim.uri_from_fname(tostring(link_match.path))
+  if IsNote[link_type] then
+    if match.note then
+      local line = match.line and match.line - 1 or 0
+      local uri = vim.uri_from_fname(tostring(match.path))
 
-    handler(nil, {
-      uri = uri,
-      range = uri and {
-        start = { line = line, character = 0 },
-        ["end"] = { line = line, character = 0 },
-      },
-    })
+      handler(nil, {
+        uri = uri,
+        range = uri and {
+          start = { line = line, character = 0 },
+          ["end"] = { line = line, character = 0 },
+        },
+      })
+    elseif IsWiki[link_type] then -- Prompt to create a new note.
+      if api.confirm("Create new note '" .. match.location .. "'?") then
+        ---@type string|?, string[]
+        local id, aliases
+        if match.name == match.location then
+          aliases = {}
+        else
+          aliases = { match.name }
+          id = match.location
+        end
+
+        local note = Note.create { title = match.name, id = id, aliases = aliases }
+        note:save()
+
+        handler(nil, {
+          uri = vim.uri_from_fname(note.path.filename),
+          range = {
+            start = { line = 0, character = 0 },
+            ["end"] = { line = 0, character = 0 },
+          },
+        })
+      end
+    end
   elseif link_type == RefTypes.FileUrl then
-    if not link_match.location then
+    if not match.location then
       return
     end
-    local path = link_match.location:sub(6)
+    local path = match.location:sub(6)
 
     handler(nil, {
       uri = vim.uri_from_fname(path),
