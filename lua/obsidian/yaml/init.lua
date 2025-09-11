c0a3cf8b9508b7d56a97fbed98e2bad0a2a03b98
neo451 <412444506@qq.com>
2025-09-11 16:09:43 +0800

    feat: fronmatter.lua

--- lua/obsidian/yaml/init.lua
+++ lua/obsidian/yaml/init.lua
@@ -95,7 +95,7 @@ end
 
 ---Dump an object to YAML lines.
 ---@param x any
----@param order function
+---@param order function?
 ---@return string[]
 yaml.dumps_lines = function(x, order)
   return dumps(x, 0, order)
