939a9b6b4d109a236d72f33b0384150d031605e2
neo451 <412444506@qq.com>
2025-09-20 16:19:57 +0100

    refactor: frontmatter validaters use go err pattern

--- lua/obsidian/frontmatter.lua
+++ lua/obsidian/frontmatter.lua
@@ -33,61 +33,69 @@ M.dump = function(t, order)
   return new_lines
 end
 
-local handlder = {}
+---@type table<string, fun(v: any, path: string): any, string?>
+local validater = {}
+M._validater = validater
 
-handlder.id = function(v, path)
+validater.id = function(v, path)
   if type(v) == "string" or type(v) == "number" then
     return tostring(v)
   else
-    log.warn("Invalid 'id' in frontmatter for " .. tostring(path))
+    return nil,
+      string.format(
+        "Invalid id '%s' in frontmatter for %s, Expected string or number found %s",
+        vim.inspect(v),
+        tostring(path),
+        type(v)
+      )
   end
 end
 
-handlder.aliases = function(v, path)
+validater.aliases = function(v, path)
   local aliases = {}
   if type(v) == "table" then
     for _, alias in ipairs(v) do
       if type(alias) == "string" then
         table.insert(aliases, alias)
       else
-        log.warn(
-          "Invalid alias value found in frontmatter for "
-            .. tostring(path)
-            .. ". Expected string, found "
-            .. type(alias)
-            .. "."
-        )
+        return nil,
+          string.format(
+            "Invalid alias '%s' in frontmatter for %s. Expected string, found %s",
+            vim.inspect(alias),
+            tostring(path),
+            type(alias)
+          )
       end
     end
   elseif type(v) == "string" then
     table.insert(aliases, v)
   else
-    log.warn("Invalid 'aliases' in frontmatter for " .. tostring(path))
+    return nil, string.format("Invalid aliases '%s' in frontmatter for %s", vim.inspect(v), tostring(path))
   end
 
   return aliases
 end
 
-handlder.tags = function(v, path)
+validater.tags = function(v, path)
   local tags = {}
   if type(v) == "table" then
     for _, tag in ipairs(v) do
       if type(tag) == "string" then
         table.insert(tags, tag)
       else
-        log.warn(
-          "Invalid tag value found in frontmatter for "
-            .. tostring(path)
-            .. ". Expected string, found "
-            .. type(tag)
-            .. "."
-        )
+        return nil,
+          string.format(
+            "Invalid tag '%s' found in frontmatter for %s. Expected string, found %s",
+            vim.inspect(tag),
+            tostring(path),
+            type(tag)
+          )
       end
     end
-  elseif type(v) == "string" then --- TODO: why not in aliases?
-    tags = vim.split(v, " ")
+  elseif type(v) == "string" then
+    table.insert(tags, v)
   else
-    log.warn("Invalid 'tags' in frontmatter for '%s'", path)
+    return nil, string.format("Invalid tags in frontmatter for '%s'", path)
   end
 
   return tags
@@ -107,8 +115,13 @@ M.parse = function(frontmatter_lines, path)
   end
   local metadata, ret = {}, {}
   for k, v in pairs(data) do
-    if handlder[k] then
-      ret[k] = handlder[k](v, path)
+    if validater[k] then
+      local value, err = validater[k](v, path)
+      if err ~= nil then
+        log.warn(err)
+      else
+        ret[k] = value
+      end
     else
       metadata[k] = v
     end
