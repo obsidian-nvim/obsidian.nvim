bf4d020e82e8678e8807a61e3458756ef4e980fc
neo451 <111681693+neo451@users.noreply.github.com>
2025-09-22 22:08:00 +0800

    refactor: `rename` reimplemented with backlinks. (#427)
    * refactor: `rename` reimplemented with backlinks.
    
    * test: proper test for cursor rename and anchor/block links
    
    ---------
    
    Co-authored-by: neo451 <412444506@qq.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -24,6 +24,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Workspace module only export one method `set`.
 - `config` is now its own module.
 - `smart_action` now properly falls back to `<CR>` unless `Obsidian.opts.checkbox.create_new`.
+- `rename` reimplemented with backlinks.
 
 ### Fixed
 
--- tests/lsp/test_rename.lua
+++ tests/lsp/test_rename.lua
@@ -8,6 +8,7 @@ local T = new_set {
       child.restart { "-u", "scripts/minimal_init_with_setup.lua" }
       child.lua [[
 Note = require"obsidian.note"
+Obsidian.opts.disable_frontmatter = true
       ]]
     end,
     post_once = function()
@@ -17,31 +18,81 @@ Note = require"obsidian.note"
   },
 }
 
-T["rename current note"] = function()
-  child.lua [==[
-target_path = tostring(Obsidian.dir / "target.md")
-vim.fn.writefile({
-  "---",
-  "id: target",
-  "---",
-  "hello",
-  "world",
-}, target_path)
+local target = [[---
+id: target
+---
+hello
+world]]
 
-referencer_path = tostring(Obsidian.dir / "referencer.md")
-vim.fn.writefile({
-  "",
-  "[[target]]",
-}, referencer_path)
+local referencer = [==[
+
+[[target]]
 ]==]
 
-  child.lua [[vim.cmd("edit " .. referencer_path)]]
+local function write(str, path)
+  vim.fn.writefile(vim.split(str, "\n"), tostring(path))
+end
+
+T["rename current note"] = function()
+  local root = Path.new(child.lua_get [[tostring(Obsidian.dir)]])
+  local target_path = root / "target.md"
+  local referencer_path = root / "referencer.md"
+  write(target, target_path)
+  write(referencer, referencer_path)
+
+  child.lua(string.format([[vim.cmd("edit %s")]], target_path))
   child.lua [[vim.lsp.buf.rename("new_target", {})]]
-  local root = child.lua_get [[tostring(Obsidian.dir)]]
-  eq(true, (Path.new(root) / "new_target.md"):exists())
+  local new_target_path = root / "new_target.md"
+  eq(true, new_target_path:exists())
   local bufs = child.api.nvim_list_bufs()
-  eq(2, #bufs)
+  eq(3, #bufs)
   local lines = child.api.nvim_buf_get_lines(1, 0, -1, false) -- new_target
+  eq("id: new_target", lines[2])
+end
+
+T["rename note under cursor"] = function()
+  local root = Path.new(child.lua_get [[tostring(Obsidian.dir)]])
+  local target_path = root / "target.md"
+  local referencer_path = root / "referencer.md"
+  write(target, target_path)
+  write(referencer, referencer_path)
+
+  child.lua(string.format([[vim.cmd("edit %s")]], referencer_path))
+  child.api.nvim_win_set_cursor(0, { 2, 0 })
+
+  child.lua [[vim.lsp.buf.rename("new_target", {})]]
+  child.lua [[vim.cmd"wa"]]
+  eq(true, (root / "new_target.md"):exists())
+  local new_target_path = root / "new_target.md"
+  local lines = vim.fn.readfile(tostring(new_target_path))
+
+  eq("id: new_target", lines[2])
+end
+
+local referencer2 = [==[
+
+[[target#^block]]
+]==]
+
+T["rename note without changing blocks and headers"] = function()
+  local root = Path.new(child.lua_get [[tostring(Obsidian.dir)]])
+  local target_path = root / "target.md"
+  local referencer_path = root / "referencer.md"
+  write(target, target_path)
+  write(referencer2, referencer_path)
+
+  child.lua(string.format([[vim.cmd("edit %s")]], referencer_path))
+  child.api.nvim_win_set_cursor(0, { 2, 0 })
+
+  child.lua [[vim.lsp.buf.rename("new_target", {})]]
+  child.lua [[vim.cmd"wa"]]
+  eq(true, (root / "new_target.md"):exists())
+  local new_target_path = root / "new_target.md"
+  local lines = vim.fn.readfile(tostring(new_target_path))
+
+  local ref_lines = vim.fn.readfile(tostring(referencer_path))
+  eq([==[[[new_target#^block]]]==], ref_lines[2])
+
   eq("id: new_target", lines[2])
 end
 
