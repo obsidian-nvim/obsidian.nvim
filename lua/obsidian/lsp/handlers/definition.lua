433f47559bc6884a21d0e05d996ac00a817dcf55
neo451 <412444506@qq.com>
2025-10-07 14:24:56 +0100

    fix: lint

--- lua/obsidian/lsp/handlers/definition.lua
+++ lua/obsidian/lsp/handlers/definition.lua
@@ -1,6 +1,4 @@
----@param _ lsp.ReferenceParams
----@param handler fun(_:any, locations: lsp.Location[])
-return function(_, handler)
+return function()
   ---@diagnostic disable-next-line: missing-fields
   require "obsidian.commands.follow_link" {}
 end
