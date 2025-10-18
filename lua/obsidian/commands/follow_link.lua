d75754f9e61a68a603aa68162e8eb53226ac1379
neo451 <412444506@qq.com>
2025-10-18 22:20:04 +0100

    fix: properly use open strat for both single and pick

--- lua/obsidian/commands/follow_link.lua
+++ lua/obsidian/commands/follow_link.lua
@@ -1,26 +1,32 @@
 local api = require "obsidian.api"
-local log = require "obsidian.log"
 
 ---@param data obsidian.CommandArgs
 return function(data)
-  local opts = {}
+  local open_strategy
   if data.args and string.len(data.args) > 0 then
-    opts.open_strategy = data.args
+    open_strategy = api.get_open_strategy(data.args)
+  else
+    open_strategy = Obsidian.opts.open_notes_in
   end
 
   vim.lsp.buf.definition {
     on_list = Obsidian.picker and function(t)
       if #t.items == 1 then
-        vim.print(t.items[1])
-        -- TODO: proper lnum
-        -- TODO: open_strategy here!
-        vim.cmd("e " .. t.items[1].filename)
+        local item = t.items[1]
+        api.open_buffer(item.filename, {
+          col = item.col,
+          line = item.lnum,
+          cmd = open_strategy,
+        })
       else
         Obsidian.picker:pick(t.items, {
           prompt_title = "Resolve link",
           callback = function(v)
-            -- TODO: open strat here?
-            api.open_buffer(v.filename, { col = v.col, line = v.lnum })
+            api.open_buffer(v.filename, {
+              col = v.col,
+              line = v.lnum,
+              cmd = open_strategy,
+            })
           end,
         })
       end
