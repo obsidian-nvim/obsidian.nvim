ac66c4f1daf612905c24ff2252942226a94d0329
neo451 <412444506@qq.com>
2025-10-19 18:07:41 +0100

    feat: default picker

---
+++ lua/obsidian/pickers/default.lua
@@ -0,0 +1,62 @@
+local Picker = require "obsidian.pickers.picker"
+local abc = require "obsidian.abc"
+local Path = require "obsidian.path"
+local api = require "obsidian.api"
+local search = require "obsidian.search"
+local log = require "obsidian.log"
+
+---@class obsidian.pickers.MiniPicker : obsidian.Picker
+local DefaultPicker = abc.new_class({
+  __tostring = function()
+    return "MiniPicker()"
+  end,
+}, Picker)
+
+--- Use input + qf for find_files TODO: no callback
+---@param opts obsidian.PickerFindOpts|? Options.
+DefaultPicker.find_files = function(self, opts)
+  opts = opts or {}
+
+  local query = api.input "Quick Switch: "
+
+  if not query then
+    return
+  end
+
+  local paths = {}
+
+  search.find_async(
+    opts.dir,
+    query,
+    {},
+    function(path)
+      paths[#paths + 1] = path
+    end,
+    vim.schedule_wrap(function(code)
+      assert(code == 0)
+
+      if vim.tbl_isempty(paths) then
+        return log.info "Failed to Switch" -- TODO:
+      elseif #paths == 1 then
+        return api.open_buffer(paths[1])
+      elseif #paths > 1 then
+        ---@type vim.quickfix.entry
+        local items = {}
+        for _, path in ipairs(paths) do
+          items[#items + 1] = {
+            filename = path,
+            lnum = 1,
+            col = 0,
+            text = self:_make_display {
+              filename = path,
+            },
+          }
+        end
+        vim.fn.setqflist(items)
+        vim.cmd "copen"
+      end
+    end)
+  )
+end
+
+return DefaultPicker
