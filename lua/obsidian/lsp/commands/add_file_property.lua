a58aa49480c10feb6a6f48bc820e18d7cc4eee69
neo451 <412444506@qq.com>
2025-11-05 01:36:38 +0000

    fix: wrong call

--- lua/obsidian/lsp/commands/add_file_property.lua
+++ lua/obsidian/lsp/commands/add_file_property.lua
@@ -7,11 +7,11 @@ return function()
   local value = obsidian.api.input "value: "
 
   if not (key and value) then
-    return obsidian.log "Aborted"
+    return obsidian.log.info "Aborted"
   end
 
   if vim.trim(key) ~= "" and vim.trim(value) ~= "" then
-    return obsidian.log "Empty Input"
+    return obsidian.log.info "Empty Input"
   end
 
   note:add_field(key, value)
