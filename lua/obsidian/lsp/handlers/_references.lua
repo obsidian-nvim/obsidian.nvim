2b366024542962d484a1c076dd5ce12af5db0622
neo451 <412444506@qq.com>
2025-11-13 19:38:59 +0000

    feat: `references` can follow tags

---
+++ lua/obsidian/lsp/handlers/_references.lua
@@ -0,0 +1,152 @@
+local util = require "obsidian.util"
+local log = require "obsidian.log"
+local api = require "obsidian.api"
+local search = require "obsidian.search"
+
+---@param match obsidian.BacklinkMatch
+---@return lsp.Location
+local function backlink_to_lsp_location(match)
+  return {
+    uri = vim.uri_from_fname(tostring(match.path)),
+    range = {
+      start = { line = match.line - 1, character = match.start },
+      ["end"] = { line = match.line - 1, character = match["end"] },
+    },
+  }
+end
+
+---@param tag_loc obsidian.TagLocation
+---@return lsp.Location
+local function tag_loc_to_lsp_location(tag_loc)
+  local line = tag_loc.line - 1
+  -- BUG: why no tags_start field??
+  local st, ed = (tag_loc.tag_start or 1) - 1, (tag_loc.tag_end or 1) - 1
+  return {
+    uri = vim.uri_from_fname(tostring(tag_loc.path)),
+    range = {
+      start = { line = line, character = st },
+      ["end"] = { line = line, character = ed },
+    },
+  }
+end
+
+---@param note obsidian.Note
+---@param opts { anchor: string|?, block: string|? }
+---@return lsp.Location[]
+local function collect_backlinks(note, opts)
+  local backlink_matches = note:backlinks { search = { sort = true }, anchor = opts.anchor, block = opts.block }
+  return vim.iter(backlink_matches):map(backlink_to_lsp_location):totable()
+end
+
+---@type table<obsidian.search.RefTypes, fun(_: string): lsp.Location[]|?>
+local handlers = {}
+
+handlers.Markdown = function(link)
+  local location = util.parse_link(link)
+  assert(location, "failed to parse link")
+
+  -- Remove block links from the end if there are any.
+  ---@type string|?
+  local block_link
+  location, block_link = util.strip_block_links(location)
+
+  -- Remove anchor links from the end if there are any.
+  ---@type string|?
+  local anchor_link
+  location, anchor_link = util.strip_anchor_links(location)
+
+  local opts = { anchor = anchor_link, block = block_link }
+
+  local notes = search.resolve_note(location)
+
+  if vim.tbl_isempty(notes) then
+    log.err("No notes matching '%s'", location)
+    return
+  else
+    local note = notes[1]
+    return collect_backlinks(note, opts)
+  end
+end
+
+handlers.Wiki = handlers.Markdown
+handlers.WikiWithAlias = handlers.Markdown
+
+handlers.Tag = function(tag)
+  local tag_locs = search.find_tags(tag)
+  if not vim.tbl_isempty(tag_locs) then
+    return vim.tbl_map(tag_loc_to_lsp_location, tag_locs)
+  end
+end
+
+local function collect_current_note(link, link_type)
+  local anchor, block
+
+  if link and link_type == "BlockID" then
+    block = util.parse_link(link)
+  end
+
+  -- Check if cursor is on a header, if so and header parsing is enabled, use that anchor.
+  -- TODO: header should just be matched as a type of "ref" by `parse_link`
+  if Obsidian.opts.backlinks.parse_headers then
+    local header_match = util.parse_header(vim.api.nvim_get_current_line())
+    if header_match then
+      anchor = header_match.anchor
+    end
+  end
+
+  local note = api.current_note(0, {
+    collect_anchor_links = anchor ~= nil,
+    collect_blocks = block ~= nil,
+  })
+
+  if note then
+    return collect_backlinks(note, { anchor = anchor, block = block })
+  else
+    log.err "Current buffer does not appear to be a note inside the vault"
+  end
+end
+
+---@type obsidian.search.RefTypes[]
+local supported_filetypes = {
+  Wiki = true,
+  WikiWithAlias = true,
+  Markdown = true,
+  Tag = true,
+  -- HeaderLink
+  -- BlockLink
+}
+
+---@param link string|?
+---@param opts { tag: boolean }
+---@param callback fun(_:any, locations: lsp.Location[])
+return function(link, opts, callback)
+  local link_type
+  if link then
+    _, _, link_type = util.parse_link(link)
+  else
+    link, link_type = api.cursor_link()
+  end
+
+  if not link and opts.tag ~= false then
+    local tag = api.cursor_tag()
+    if tag then
+      link = tag
+      link_type = "Tag"
+    end
+  end
+
+  if not link or not link_type or not supported_filetypes[link_type] then
+    callback(nil, collect_current_note(link, link_type))
+    return
+  end
+
+  local locations
+
+  if handlers[link_type] then
+    locations = handlers[link_type](link)
+  end
+
+  if locations then
+    callback(nil, locations)
+  end
+end
--- tests/lsp/test_references.lua
+++ tests/lsp/test_references.lua
@@ -23,6 +23,27 @@ T["find wiki references"] = function()
   eq("[[target]]", qflist[1].text)
 end
 
+T["find wiki references under cursor"] = function()
+  local referencer = [==[
+
+[[target]]
+]==]
+
+  local root = child.Obsidian.dir
+  local referencer_path = root / "referencer.md"
+  h.write(referencer, referencer_path)
+
+  local target_path = root / "target.md"
+  h.write("", target_path)
+
+  child.cmd(string.format("edit %s", referencer_path))
+  child.api.nvim_win_set_cursor(0, { 2, 0 })
+  child.lua "vim.lsp.buf.references()"
+  local qflist = child.fn.getqflist()
+  eq(1, #qflist)
+  eq("[[target]]", qflist[1].text)
+end
+
 T["find markdown references"] = function()
   local referencer = [==[
 
@@ -43,7 +64,48 @@ T["find markdown references"] = function()
   eq("[target](target.md)", qflist[1].text)
 end
 
-T["resolve header links"] = function()
+T["find markdown references under cursor"] = function()
+  local referencer = [==[
+
+[target](target.md)
+]==]
+
+  local root = child.Obsidian.dir
+  local referencer_path = root / "referencer.md"
+  h.write(referencer, referencer_path)
+
+  local target_path = root / "target.md"
+  h.write("", target_path)
+
+  child.cmd(string.format("edit %s", referencer_path))
+  child.api.nvim_win_set_cursor(0, { 2, 0 })
+  child.lua "vim.lsp.buf.references()"
+  local qflist = child.fn.getqflist()
+  eq(1, #qflist)
+  eq("[target](target.md)", qflist[1].text)
+end
+
+T["find tag references under cursor"] = function()
+  local file = [==[
+
+#tag
+
+#tag
+]==]
+
+  local root = child.Obsidian.dir
+  local file_path = root / "file.md"
+  h.write(file, file_path)
+
+  child.cmd(string.format("edit %s", file_path))
+  child.api.nvim_win_set_cursor(0, { 2, 0 })
+  child.lua "vim.lsp.buf.references()"
+  local qflist = child.fn.getqflist()
+  eq(2, #qflist)
+  eq("#tag", qflist[1].text)
+end
+
+T["resolve header links under cursor"] = function()
   local referencer = [==[
 
 
@@ -81,6 +143,47 @@ T["resolve header links"] = function()
   eq("[[target#header]]", qflist[1].text)
   eq(3, qflist[1].lnum)
 end
+
+T["resolve blocks under cursor"] = function()
+  local file = [==[
+
+[[file#^123]]
+
+block ^123
+]==]
+
+  local root = child.Obsidian.dir
+  local file_path = root / "file.md"
+  h.write(file, file_path)
+
+  child.cmd(string.format("edit %s", file_path))
+  child.api.nvim_win_set_cursor(0, { 2, 0 })
+  child.lua "vim.lsp.buf.references()"
+  local qflist = child.fn.getqflist()
+  eq(1, #qflist)
+  eq("[[file#^123]]", qflist[1].text)
+end
+
+-- TODO:
+-- T["find block references under cursor"] = function()
+--   local file = [==[
+--
+-- [[#^123]]
+--
+-- block ^123
+-- ]==]
+--
+--   local root = child.Obsidian.dir
+--   local file_path = root / "file.md"
+--   h.write(file, file_path)
+--
+--   child.cmd(string.format("edit %s", file_path))
+--   child.api.nvim_win_set_cursor(0, { 2, 0 })
+--   child.lua "vim.lsp.buf.references()"
+--   local qflist = child.fn.getqflist()
+--   eq(1, #qflist)
+--   eq("[[file#^123]]", qflist[1].text)
+-- end
 
 T["avoid invalid patterns"] = function()
   local referencer = [==[
