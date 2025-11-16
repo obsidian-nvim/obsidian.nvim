b39422089672e01e94e1d6b2b56604c69495722c
neo451 <412444506@qq.com>
2025-11-16 15:29:33 +0000

    wip: move towards a clean wiki implementation

--- lua/obsidian/util.lua
+++ lua/obsidian/util.lua
@@ -497,11 +497,6 @@ end
 ------------------------------------
 -- Miscellaneous helper functions --
 ------------------------------------
----@param anchor obsidian.note.HeaderAnchor
----@return string
-util.format_anchor_label = function(anchor)
-  return string.format(" ❯ %s", anchor.header)
-end
 
 -- We are very loose here because obsidian allows pretty much anything
 util.ANCHOR_LINK_PATTERN = "#[%w%d\128-\255][^#]*"
