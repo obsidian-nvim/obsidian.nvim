03e8cd5263e731258ec4d521994dc6568ddb0f77
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-01-06 01:07:47 +0000

    improve add_property

--- lua/obsidian/lsp/commands/add_file_property.lua
+++ lua/obsidian/lsp/commands/add_file_property.lua
@@ -1,19 +1,3 @@
 local obsidian = require "obsidian"
 
-return function()
-  local note = assert(obsidian.api.current_note(0))
-
-  local key = obsidian.api.input "key: "
-  local value = obsidian.api.input "value: "
-
-  if not (key and value) then
-    return obsidian.log.info "Aborted"
-  end
-
-  if vim.trim(key) ~= "" and vim.trim(value) ~= "" then
-    return obsidian.log.info "Empty Input"
-  end
-
-  note:add_field(key, value)
-  note:update_frontmatter(0)
-end
+return obsidian.api.add_property
