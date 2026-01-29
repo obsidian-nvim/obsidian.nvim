11757ae3c85c21f6d75abe716f8c63e86339762f
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-01-29 19:11:39 +0000

    fix: rename

--- lua/obsidian/commands/rename.lua
+++ lua/obsidian/commands/rename.lua
@@ -1,7 +1,8 @@
 ---@param data obsidian.CommandArgs
 return function(data)
-  local new_name = vim.trim(data.args)
-  if string.len(new_name) then
-    require("obsidian.actions").rename(new_name)
+  local new_name
+  if string.len(new_name) > 0 then
+    new_name = vim.trim(data.args)
   end
+  require("obsidian.actions").rename(new_name)
 end
