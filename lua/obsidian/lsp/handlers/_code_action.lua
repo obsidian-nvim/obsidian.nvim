82e61e8197df6ea16cf5903048ae01ca66a77537
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-02-01 19:55:38 +0000

    cleanup

--- lua/obsidian/lsp/handlers/_code_action.lua
+++ lua/obsidian/lsp/handlers/_code_action.lua
@@ -1,86 +1,89 @@
----@alias obsidian.lsp.CodeAtionDefaults
----| "rename"
----| "insert_template"
----| "add_property"
----| "link"
----| "link_new"
----| "extract_note"
+local actions = require "obsidian.actions"
 
 ---@type table<string, lsp.CodeAction>
-local actions = {}
+local code_actions = {}
 
 ---@class obsidian.lsp.CodeActionOpts
----@field name string | obsidian.lsp.CodeAtionDefaults internal server command name, recommend to keep to snake case
+---@field name string  internal server command name, recommend to keep to snake case
 ---@field title string text display in code action interface
----@field fn function|? function to run
+---@field fn function function to run
 ---@field range boolean|? whether to show action only in visual mode
 
 ---Register a new command.
----@param config obsidian.lsp.CodeActionOpts
-local add = function(config)
-  local fn = config.fn or require("obsidian.actions")[config.name]
-  if not fn then
-    -- TODO:
-    return
-  end
+---@param opts obsidian.lsp.CodeActionOpts
+local add = function(opts)
+  -- TODO: validate
   local action = {
-    title = config.title,
+    title = opts.title,
     command = {
-      title = config.title,
-      command = config.name,
+      title = opts.title,
+      command = opts.name,
       -- TODO: kind
     },
     data = {
-      range = config.range,
-      fn = fn,
-      -- TODO: preview edit
+      range = opts.range,
+      fn = opts.fn,
+      -- TODO: preview edit with preview_fn
     },
   }
-  actions[config.name] = action
+  code_actions[opts.name] = action
 end
+
+---@enum (key) obsidian.lsp.CodeAtionDefaults
+local default_actions = {
+  rename = {
+    name = "rename",
+    title = "Rename current note",
+    fn = actions.rename,
+  },
+
+  insert_template = {
+    name = "insert_template",
+    title = "Insert template at cursor",
+    fn = actions.insert_template,
+  },
+
+  add_property = {
+    name = "add_property",
+    title = "Add file property",
+    fn = actions.add_property,
+  },
+
+  link = {
+    name = "link",
+    title = "Link selection as name for a existing note",
+    fn = actions.link,
+    range = true,
+  },
+
+  link_new = {
+    name = "link_new",
+    title = "Link selection as name for a new note",
+    fn = actions.link_new,
+    range = true,
+  },
+
+  extract_note = {
+    name = "extract_note",
+    title = "Extract selected text to a new note",
+    fn = actions.extract_note,
+    range = true,
+  },
+}
 
 -- TODO: merge a note to this note, after https://github.com/obsidian-nvim/obsidian.nvim/issues/655
 
-add {
-  name = "rename",
-  title = "Rename current note",
-}
-
-add {
-  name = "insert_template",
-  title = "Insert template at cursor",
-}
-
-add {
-  name = "add_property",
-  title = "Add file property",
-}
-
-add {
-  name = "link",
-  title = "Link selection as name for a existing note",
-  range = true,
-}
-
-add {
-  name = "link_new",
-  title = "Link selection as name for a new note",
-  range = true,
-}
-
-add {
-  name = "extract_note",
-  title = "Extract selected text to a new note",
-  range = true,
-}
-
 ---@param name string | obsidian.lsp.CodeAtionDefaults
 local del = function(name)
-  actions[name] = nil
+  code_actions[name] = nil
+end
+
+for _, action in pairs(default_actions) do
+  add(action)
 end
 
 return {
-  actions = actions,
+  actions = code_actions,
   add = add,
   del = del,
 }
