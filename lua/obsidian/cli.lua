0f78039b50052b24104307d365712f603771aa92
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-02-28 11:42:07 +0000

    feat: cli class for headless sync and future cli operations

---
+++ lua/obsidian/cli.lua
@@ -0,0 +1,53 @@
+---@class obsidian.CLI
+local M = {}
+
+M.__index = M
+
+---@class obsidian.CLIOpts
+---@field callback? fun(out: vim.SystemCompleted)
+---@field silent? boolean
+
+---@param cmd string
+---@param opts obsidian.CLIOpts
+---@return obsidian.CLI
+M.new = function(cmd, opts)
+  return setmetatable({
+    cmd = cmd,
+    opts = opts,
+  }, M)
+end
+
+---@param subcmd string
+---@param flags table<string, string>|?
+---@param opts obsidian.CLIOpts|?
+---@return vim.SystemObj
+M.run = function(self, subcmd, flags, opts)
+  local cmds = { self.cmd, subcmd }
+  flags = flags or {}
+  opts = opts or {}
+
+  local callback = self.opts.callback or opts.callback
+  local silent = self.opts.silent or opts.silent -- TODO: this is a bit hacky, maybe we should merge opts in the constructor instead?
+
+  for k, v in pairs(flags) do
+    table.insert(cmds, string.format("--%s", k))
+    table.insert(cmds, v)
+  end
+
+  return vim.system(cmds, {}, function(out)
+    if not silent and out.code ~= 0 then
+      vim.notify(string.format("Command failed: %s", table.concat(cmds, " ")), vim.log.levels.ERROR)
+      return
+    end
+
+    if callback then
+      callback(out)
+    end
+  end)
+end
+
+-- local cli = M.new("ob", {})
+--
+-- cli:run("sync", {
+--   workspace = "my_workspace",
+-- })
