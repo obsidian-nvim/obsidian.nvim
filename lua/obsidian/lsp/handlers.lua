8d165b77ae7a34e3a173fc8e1e66cca2493684e6
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-03-07 23:55:19 +0000

    Trivial fixes

--- lua/obsidian/lsp/handlers.lua
+++ lua/obsidian/lsp/handlers.lua
@@ -1,14 +1,9 @@
-local ms = vim.lsp.protocol.Methods
-
-return setmetatable({
-  [ms.initialize] = require "obsidian.lsp.handlers.initialize",
-  [ms.textDocument_rename] = require "obsidian.lsp.handlers.rename",
-  [ms.textDocument_prepareRename] = require "obsidian.lsp.handlers.prepare_rename",
-  [ms.textDocument_references] = require "obsidian.lsp.handlers.references",
-  [ms.textDocument_definition] = require "obsidian.lsp.handlers.definition",
-  [ms.textDocument_documentSymbol] = require "obsidian.lsp.handlers.document_symbol",
-}, {
-  __index = function(_, _)
-    return function() end
-  end,
-})
+---@type table<vim.lsp.protocol.Method, function>
+return {
+  ["initialize"] = require "obsidian.lsp.handlers.initialize",
+  ["textDocument/rename"] = require "obsidian.lsp.handlers.rename",
+  ["textDocument/prepareRename"] = require "obsidian.lsp.handlers.prepare_rename",
+  ["textDocument/references"] = require "obsidian.lsp.handlers.references",
+  ["textDocument/definition"] = require "obsidian.lsp.handlers.definition",
+  ["textDocument/documentSymbol"] = require "obsidian.lsp.handlers.document_symbol",
+}
