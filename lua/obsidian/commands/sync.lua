67e3bb9c662df216e02afab593aa5ce39bf476a0
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-04-05 01:59:12 +0100

    check configured

--- lua/obsidian/commands/sync.lua
+++ lua/obsidian/commands/sync.lua
@@ -1,7 +1,22 @@
+local sync = require "obsidian.sync"
+local api = require "obsidian.api"
+
 ---@param data obsidian.CommandArgs
 return function(data)
   local subcmd = data.args:len() > 0 and data.args or nil
   -- TODO: doc on how this command behaves
-  -- TODO: if sync.enabled and no conigured vault -> run wizard
+
+  -- TODO: or not logged in, or no vaults configured for syncing
+  local has_configured = vim.iter(Obsidian.workspaces):any(sync.is_configured)
+
+  if not has_configured then
+    local choice = api.confirm "No vaults are configured for syncing. Do you want to run the setup wizard?"
+    if choice == "Yes" then
+      require("obsidian.setup").wizard()
+    else
+      return
+    end
+  end
+
   require("obsidian.sync").menu(subcmd)
 end
