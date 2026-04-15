9f7d9625898cf62c32226681142eb49ab6dbed7a
neo451 <412444506@qq.com>
2026-04-15 19:24:48 +0100

    minimal.lua

--- minimal.lua
+++ minimal.lua
@@ -7,6 +7,23 @@ vim.o.conceallevel = 2
 
 local cwd = vim.uv.cwd()
 
+local chars = {}
+for i = 32, 126 do
+  table.insert(chars, string.char(i))
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
+    if client and client.name == "obsidian-ls" then
+      client.server_capabilities.completionProvider.triggerCharacters = chars
+      -- vim.bo[ev.buf].completeopt = "menuone,noselect,fuzzy,nosort" -- noselect to make sure no accidentally accept and create new notes, others are not necessary
+      -- vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
+
 local plugins = {
   {
     "obsidian-nvim/obsidian.nvim",
@@ -35,12 +52,18 @@ local plugins = {
   -- **Choose your completion engine**
   -- {
   --   "hrsh7th/nvim-cmp",
+  --   dependencies = {
+  --     "hrsh7th/cmp-nvim-lsp",
+  --   },
   --   config = function()
   --     local cmp = require "cmp"
   --     cmp.setup {
   --       mapping = cmp.mapping.preset.insert {
   --         ["<C-e>"] = cmp.mapping.abort(),
   --         ["<C-y>"] = cmp.mapping.confirm { select = true },
+  --       },
+  --       sources = {
+  --         { name = "nvim_lsp" },
   --       },
   --     }
   --   end,
@@ -52,9 +75,13 @@ local plugins = {
   --   },
   -- },
   -- {
-  --   "nvim-mini/mini.completion",
-  --   opts = {},
+  --   "nvim-mini/mini.nvim",
+  --   config = function()
+  --     require("mini.completion").setup {}
+  --   end,
   -- },
 }
 
 require("lazy.minit").repro { spec = plugins }
+
+-- vim.cmd "checkhealth obsidian"
