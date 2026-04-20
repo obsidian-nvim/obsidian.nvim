4c03091325abcdc8879365c2c04ee4c025c560d9
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-04-20 21:07:39 +0100

    rename

--- lua/obsidian/commands/rename.lua
+++ lua/obsidian/commands/rename.lua
@@ -4,5 +4,5 @@ return function(data)
   if string.len(new_name) > 0 then
     new_name = vim.trim(data.args)
   end
-  require("obsidian.actions").rename(new_name)
+  vim.lsp.buf.rename(new_name)
 end
