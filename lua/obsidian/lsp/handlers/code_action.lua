ae237c1f17a2f2cc89511a1300a780077e84366d
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-05-21 16:03:22 +0100

    feat(actions): add_bookmark

--- lua/obsidian/lsp/handlers/code_action.lua
+++ lua/obsidian/lsp/handlers/code_action.lua
@@ -1,26 +1,16 @@
-local actions = require("obsidian.lsp.handlers._code_action").actions
+local _ca = require "obsidian.lsp.handlers._code_action"
+local actions = _ca.actions
+local resolve = _ca.resolve
 
----@param title string|fun(note: obsidian.Note): string
----@param note obsidian.Note
----@return string
-local function eval_title(title, note)
-  if type(title) == "function" then
-    return title(note)
-  end
-  return title
-end
-
----@param code_actions table<string, obsidian.lsp.CodeAction>
+---@param code_actions table<string, obsidian.lsp.CodeActionOpts>
 ---@param note obsidian.Note
 ---@return lsp.CodeAction[]
 local function get_commands_by_context(code_actions, note)
   local out = {}
-  for _, code_action in ipairs(vim.tbl_values(code_actions)) do
-    local data = code_action.data
-    if data.cond(note) then
-      local title = eval_title(data.title, note)
-      local command = vim.tbl_extend("force", code_action.command or {}, { title = title })
-      out[#out + 1] = vim.tbl_extend("force", code_action, { title = title, command = command })
+  for _, opts in ipairs(vim.tbl_values(code_actions)) do
+    local cond = opts.cond
+    if not cond or cond(note) then
+      out[#out + 1] = resolve(opts, note)
     end
   end
   return out
