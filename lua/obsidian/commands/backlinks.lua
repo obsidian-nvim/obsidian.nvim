cb2865dd223e80573ea783de10dcb46aed4a6e33
dingbat-rascal <dingbat.rascal@gmail.com>
2026-05-24 14:16:01 -0700

    Update backlinks.lua
    formatted

--- lua/obsidian/commands/backlinks.lua
+++ lua/obsidian/commands/backlinks.lua
@@ -2,24 +2,25 @@ local obsidian = require "obsidian"
 
 return function()
   require "obsidian.lsp.handlers._references"(nil, { tag = false }, function(_, locations)
-        local items = vim.lsp.util.locations_toi_items(locations, "utf-8")
-      if #items == 0 then
-        obsidian.log.info ("No backlinks")
-        return
-      end
-      if #items == 1 then
-        obsidian.api.open_note(items[1])
+    local items = vim.lsp.util.locations_toi_items(locations, "utf-8")
+    if #items == 0 then
+      obsidian.log.info "No backlinks"
+      return
+    end
+    if #items == 1 then
+      obsidian.api.open_note(items[1])
     else
-      Obsidian.picker.pick(items, { prompt_title = "Resolve link",
-          format_item = function (item)
-            return vim.fs.basename(item.filename) .. ":" .. item.lnum .. " - " .. vim.trim(item.text)
-          end,
-          callback = function(selected_item)
-            if selected_item then
-              obsidian.api.open_note(selected_item)
-            end
+      Obsidian.picker.pick(items, {
+        prompt_title = "Resolve link",
+        format_item = function(item)
+          return vim.fs.basename(item.filename) .. ":" .. item.lnum .. " - " .. vim.trim(item.text)
+        end,
+        callback = function(selected_item)
+          if selected_item then
+            obsidian.api.open_note(selected_item)
           end
-        })
+        end,
+      })
     end
   end)
 end
