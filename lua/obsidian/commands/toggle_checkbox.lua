95c2c7b1d6b51dc09960d63f7d4dcf33bd378fb1
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-06-05 23:11:24 +0100

    fix(commands): properly exporet toggle_checkbox command

--- lua/obsidian/commands/toggle_checkbox.lua
+++ lua/obsidian/commands/toggle_checkbox.lua
@@ -1,18 +1,4 @@
-local toggle_checkbox = require("obsidian.api").toggle_checkbox
-
 ---@param data obsidian.CommandArgs
 return function(data)
-  local start_line, end_line
-  local checkboxes = Obsidian.opts.checkbox.order
-  start_line = data.line1
-  end_line = data.line2
-
-  local buf = vim.api.nvim_get_current_buf()
-
-  for line_nb = start_line, end_line do
-    local current_line = vim.api.nvim_buf_get_lines(buf, line_nb - 1, line_nb, false)[1]
-    if current_line and (current_line:match "%S" or Obsidian.opts.checkbox.create_new) then
-      toggle_checkbox(checkboxes, line_nb)
-    end
-  end
+  require("obsidian.actions").toggle_checkbox(data.line1, data.line2)
 end
