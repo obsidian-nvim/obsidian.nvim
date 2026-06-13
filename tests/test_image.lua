4d9c4009754ebfae630389cb63fa865ae4fe4969
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-06-13 17:41:16 +0100

    resize

--- tests/test_image.lua
+++ tests/test_image.lua
@@ -48,15 +48,54 @@ T["inline"]["renders a fitted image below the image link"] = function()
       return #calls > 0
     end)
 
-    eq(pos.row + 1, calls[1].opts.row)
-    eq(pos.col, calls[1].opts.col)
-    assert(calls[1].opts.width <= 10)
-    assert(calls[1].opts.height <= 5)
+    eq("buffer", calls[1].opts.relative)
+    eq(1, calls[1].opts.row)
+    eq(1, calls[1].opts.col)
+    eq(pos.col - 1, calls[1].opts.pad)
+    assert(calls[1].opts.width <= 10, "image width should fit max_width")
+    assert(calls[1].opts.height <= 5, "image height should fit max_height")
+  end)
 
-    local ns = vim.api.nvim_get_namespaces()["obsidian.image"]
-    local extmarks = vim.api.nvim_buf_get_extmarks(0, ns, 0, -1, { details = true })
-    eq(1, #extmarks)
-    eq(calls[1].opts.height, #extmarks[1][4].virt_lines)
+  image.detach(0)
+  vim.ui.img = original_img
+
+  if not ok then
+    error(err)
+  end
+end
+
+T["inline"]["uses Obsidian embed dimensions"] = function()
+  local image = require "obsidian.image"
+  local original_img = vim.ui.img
+  local calls = {}
+
+  vim.ui.img = {
+    set = function(data_or_id, opts)
+      calls[#calls + 1] = { data_or_id = data_or_id, opts = opts }
+      return #calls
+    end,
+    get = function(id)
+      return calls[id] and calls[id].opts
+    end,
+    del = function()
+      return true
+    end,
+  }
+
+  local ok, err = pcall(function()
+    local img_path = vim.fs.joinpath(tostring(Obsidian.dir), "sized.png")
+    local note_path = vim.fs.joinpath(tostring(Obsidian.dir), "sized-note.md")
+    write_png_header(img_path, 900, 360)
+    vim.api.nvim_buf_set_name(0, note_path)
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, { "![[sized.png|90x36]]" })
+
+    image.attach(0, { debounce = 0, visible_only = true, max_width = 1, max_height = 1 })
+    h.wait(function()
+      return #calls > 0
+    end)
+
+    assert(calls[1].opts.width > 1, "embed width should override max_width")
+    eq("buffer", calls[1].opts.relative)
   end)
 
   image.detach(0)
