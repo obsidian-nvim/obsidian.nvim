a621cc5f9100c5a2edc07e3314218f099299fe2c
zizhou teng (n451) <2020200706@ruc.edu.cn>
2026-06-27 01:31:14 +0100

    cleanup

--- lua/obsidian/picker/init.lua
+++ lua/obsidian/picker/init.lua
@@ -314,6 +314,16 @@ M.find_files_from_cache = function(opts)
       pick_query = nil
     end
 
+    ---@param entry obsidian.PickerEntry
+    ---@return string
+    local format_picker_entry = function(entry)
+      local icon = icons.get_icon(entry)
+      local text = entry.text or ""
+      return icon .. " " .. text
+    end
+
+    -- TODO: preview_item
+
     M.select(entries, {
       prompt = opts.prompt_title,
       allow_multiple = true,
@@ -322,7 +332,7 @@ M.find_files_from_cache = function(opts)
       query = pick_query,
       query_mappings = opts.query_mappings,
       selection_mappings = opts.selection_mappings,
-      format_item = icons.format_picker_entry,
+      format_item = format_picker_entry,
     }, function(items)
       local paths = vim.tbl_filter(
         function(path)
